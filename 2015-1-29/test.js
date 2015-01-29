c125abb3b5d82975cd50617e7b27abd2b193a3c5
DorisDong <sqcdong1989@163.com>
2015-01-29 17:38:56 +0800

    CSS入门，CSS选择器

---
+++ 2015-1-29/test.js
@@ -0,0 +1,28 @@
+/* var height = "about 5'10\" tall ";
+var temp_string=10+"20";
+var temp_string2= 10 + 20;
+//alert(temp_string);
+var count=1;
+while(count<11){
+
+    count ++ ;
+}
+//alert(count);
+*/
+function shout(){
+    var beatles =Array ("john","Paul","George","Ring");
+    for( var count =0 ;count<beatles.length;count++){
+        //alert(beatles[count]);
+        console.log(beatles[count]);
+    }
+}
+
+shout();
+
+//function multiply(num1,num2){
+//    var total =num1 * num2 ;
+//    return total ;
+//}
+////var total;
+////multiply(2,3);
+//alert(multiply(2,3));
